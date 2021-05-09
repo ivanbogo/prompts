4872ed7675e7c1f9988721e87be6503be215421d
ivan <ivan@home>
2021-05-09 11:37:28 +0100

    Cursor lock

--- p.py
+++ p.py
@@ -30,7 +30,7 @@ class MyControl(UIControl):
 
     def get_line(self, i):
         if self.m.numbers:
-            return [(Token.LineNumber, '%03d ' % i), (Token.Text, self.m.get_line(i))]
+            return [(Token.LineNumber, '%6d ' % i), (Token.Text, self.m.get_line(i))]
         else:
             return [(Token.Text, self.m.get_line(i))]
 
@@ -41,6 +41,7 @@ class MyControl(UIControl):
 class Tail:
     def __init__(self, path):
         self.edit = False
+        self.lock = False
 
         self.model = model.Model(path)
         self.style = style_from_dict({
@@ -49,10 +50,11 @@ class Tail:
 
         self.wrapping = Condition(lambda cli: self.model.wrap)
         self.editing = Condition(lambda cli: self.edit)
+        self.locked = Condition(lambda clie: self.lock)
 
         self.main = Window(
             content=MyControl(self.model),
-            cursorline=True,
+            cursorline=~self.locked,
             wrap_lines=self.wrapping)
 
         self.status = Window(
@@ -82,11 +84,11 @@ class Tail:
             (self.edit, 'e'), (self.model.wrap, 'w'), (self.model.numbers, 'n')) if c[0]])
 
         return [
-            (Token.Status, status),
+            (Token.Status, '[%4s]' % status),
             (Token.Text, ' '),
             (Token.Text, self.model.path),
             (Token.Text, ': '),
-            (Token.LineNumber, '%d/%d' % (self.model.line, self.model.line_count()))
+            (Token.LineNumber, '%d,%d/%d' % (self.model.line, self.main.vertical_scroll, self.model.line_count()))
         ]
 
     def run(self):
@@ -97,13 +99,25 @@ class Tail:
         finally:
             eventloop.close()
 
-    def scroll(self, count):
-        y = self.model.line = max(0, min(self.model.line_count(), self.model.line + count))
-        self.main.content.content.cursor_position = Point(y, 0)
-        self.main.vertical_scroll = y
+    def scroll(self, count, half):
+        info = self.main.render_info
+        if not info:
+            return
+
+        if half is not None:
+            count *= info.window_height
+            if half:
+                count = count // 2
+
+        self.main.vertical_scroll = max(0, min(self.model.line_count() - 1, self.main.vertical_scroll + count))
+        if self.model.line < self.main.vertical_scroll or \
+                self.model.line > self.main.vertical_scroll + info.window_height:
+
+            y = self.model.line = self.main.vertical_scroll
+            self.main.content.content.cursor_position = Point(y, 0)
 
     def move_cursor(self, count):
-        y = self.model.line = max(0, min(self.model.line_count(), self.model.line + count))
+        y = self.model.line = max(0, min(self.model.line_count() - 1, self.model.line + count))
         self.main.content.content.cursor_position = Point(y, 0)
 
         if not self.main.render_info:
@@ -113,22 +127,42 @@ class Tail:
         info = w.render_info
 
         if y > w.vertical_scroll + info.window_height or y < w.vertical_scroll:
-            w.vertical_scroll = y
+            w.vertical_scroll = max(0, y - info.window_height // 2)
 
     def load_bindings(self):
         bindings = load_key_bindings(enable_abort_and_exit_bindings=True, enable_system_bindings=True)
 
         @bindings.add_binding('j', filter=~self.editing)
-        def down(event): self.scroll(1)
+        def down(event):
+            if self.lock:
+                self.scroll(1, None)
+            else:
+                self.move_cursor(1)
 
         @bindings.add_binding('k', filter=~self.editing)
-        def up(event): self.scroll(-1)
+        def up(event):
+            if self.lock:
+                self.scroll(-1, None)
+            else:
+                self.move_cursor(-1)
+
+        @bindings.add_binding('g', filter=~self.editing)
+        def c_d(event): self.scroll(-self.model.line_count(), None)
+
+        @bindings.add_binding('G', filter=~self.editing)
+        def c_d(event): self.scroll(self.model.line_count(), None)
 
         @bindings.add_binding('d', filter=~self.editing)
-        def c_d(event): self.move_cursor(20)
+        def c_d(event): self.scroll(1, True)
+
+        @bindings.add_binding('f', filter=~self.editing)
+        def c_d(event): self.scroll(1, False)
 
         @bindings.add_binding('u', filter=~self.editing)
-        def c_u(event): self.move_cursor(-20)
+        def c_u(event): self.scroll(-1, True)
+
+        @bindings.add_binding('b', filter=~self.editing)
+        def c_u(event): self.scroll(-1, False)
 
         @bindings.add_binding('w', filter=~self.editing)
         def toggle_wrap(event): self.model.wrap = not self.model.wrap
@@ -144,6 +178,9 @@ class Tail:
 
         @bindings.add_binding(Keys.ControlC)
         def end_edit(event): self.edit = False
+
+        @bindings.add_binding('l', filter=~self.editing)
+        def _(event): self.lock = not self.lock
 
         return bindings
 
